7e7ede8711cf3b0ba755d08e5d40911df8b98da8
Johnathnat <johnathanfbarker@hotmail.com>
2022-10-27 13:27:27 +0100

    what i want my code to do

--- src/Week4/Week04.java
+++ src/Week4/Week04.java
@@ -13,8 +13,27 @@ public class Week04
         System.out.println();
         System.out.println("  Week 4 Java Collections, Lists, Arrays ");
         System.out.println();
-        System.out.println(" by Nicholas Day and Derek Peacock");
+        System.out.println(" by Johnathan Barker");
         System.out.println();
     }     
     
+//what I want the code to do
+
+//function make list of songs
+
+//function table
+    // output song list so that it looks like a table.
+    // Possibly getting information from the internet
+
+//function adding to the table
+    // table is made up of an array so just find out how to add to the array.
+    // ask the user for the name of the song
+    // ask user for the person who sings the song
+    // look up what the number of plays is using the internet.
+
+//fuction taking away from the table.
+    //inverse of adding
+    //have a are you sure option
+
+
 }
