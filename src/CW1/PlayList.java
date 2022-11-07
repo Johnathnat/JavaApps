f331ff2bff946f17ff63c165c902a58bd2aae819
Johnathan Barker <114746425+Johnathnat@users.noreply.github.com>
2022-11-07 13:23:57 +0000

    Update PlayList.java

--- src/CW1/PlayList.java
+++ src/CW1/PlayList.java
@@ -1,179 +1,206 @@
 package CW1;
-import helpers.InputReader;
-import java.io.*;
-import java.lang.*;
-import java.util.*;
 
-/**Main class*/
+import helpers.InputReader;
+
+import java.util.List;
+
+
+
+
 public class PlayList
 {
     /** Declaring an array of Songs*/
-    private Song[] songs;
+    private CW1.Song[] arr;
 
     public void run()
     {
-    /**
-            Allocating memory for objects
-            of type Songs
-         */
-        songs = new Song[100];
+
+
+        /**
+         * Allocating memory for 11 type of Songs
+         * It should be more but for some reason It won't work*/
+        arr = new CW1.Song[11];
 
         stockSongs();
+        menuSelection();
 
         //amount of songs
-        //int NoOfSongs = songs.length;
-        System.out.println(songs.length);
 
-        addSongs();
-        printList();
-        removeSongs();
-        printList();
+        System.out.println(arr.length);
+
+        //boolean wantToRemoveSong = removeSong();
     }
-    
-    
-    
-/**   
-This was some code I found to try and add the songs to the array but it didnt end up working
-
-      Function to add x in arr
-      public static int[] addX(int n, int songs[], int x)
-      {
-          int i;
-  
-          // create a new array of size n+1
-          int newarr[] = new int[n + 1];
-  
-          // insert the elements from
-          // the old array into the new array
-          // insert all elements till n
-          // then insert x at n+1
-          for (i = 0; i < n; i++)
-              newarr[i] = songs[i];
-  
-          newarr[n] = x;
-  
-          return newarr;
-     }
-     */
-    private void addSongs() {
-        /**
-          User added song
-          */
-        String newSongName = InputReader.getString("Enter song name: ");
-        int newNumberOfPlays = InputReader.getInt("Enter Number of Plays: ");
-
-
-        
-        int index = getLastSong()+1;
-        songs[index] = new Song(newNumberOfPlays, newSongName);
-        //var addedsong = new Song(newNumberOfPlays, newSongName);
-        //songs = addX(addedsong);
-
-    }
-/**This was an attempt to be able to cound the amount of songs in the List but for some
-reason.*/
-    private int getLastSong()
+//print list
+    public void printList()
     {
-        int count = 0;
-
-        for(Song song : songs)
-        {
-            if(song == null)
-                return count;
-            else count++;
-        }
-        return count;
-    }
-    
-    /**
-     * Removing a song
-    */
-    private void removeSongs() {
-        int songNumber = songs.length;
-
-        while (songNumber < songs.length) {
-
-            System.out.println("Song " + songNumber + ":");
-            songNumber = songNumber - 1;
-            songs[songNumber].display();
-        }
-        //int numberOfSong = InputReader.getInt("What song number do you want to remove?");
-
-        songs[numberOfRemovedSong()] = null;
-    }
-
-/**
- * print list
- * */
-    private void printList() {
-        System.out.println(songs.length);
+        stockSongs();
+        System.out.println(arr.length);
         int songNumber = 0;
-        //int fixedNumberOfSong = numberOfRemovedSong();
-        while (songNumber < 100) {
-
-
-
-           // if (songNumber == fixedNumberOfSong) continue;
-            System.out.print(songNumber + " ");
-            System.out.println("Song " + songNumber + ":");
-            songs[songNumber].display();
+        int fixedNumberOfSong = numberOfRemovedSong()+1;
+        while (songNumber<arr.length) {
             songNumber = songNumber + 1;
-        }
+
+            if(songNumber == fixedNumberOfSong) continue;
+
+            //System.out.print(songNumber +" ");
+            System.out.println("Song " + songNumber+":");
+            arr[songNumber-1].display();
+
     }
 //Sort List
-       // https://www.geeksforgeeks.org/arrays-sort-in-java-with-examples/
+        // https://www.geeksforgeeks.org/arrays-sort-in-java-with-examples/
+    }
 
+    private void sortSongs()
+    {
+        int numberofPlays=7;
+        //int numberofPlays = Song.getNumberOfPlays();
+        int wantedRange = InputReader.getInt("Enter the number of play you want the songs to be above");
+        if (numberofPlays<=wantedRange)
+        {
+            int songNumber = 0;
 
+            while (songNumber<arr.length) {
+                songNumber = songNumber + 1;
+
+                if (songNumber == wantedRange) continue;
+
+                //System.out.print(songNumber +" ");
+                System.out.println("Song " + songNumber + ":");
+                arr[songNumber-1].display();
+            }
+        }
+
+    }
 
 
     private void stockSongs()
     {
-        songs[0] = new Song(59966519, "Bushes of love");
-        songs[1] = new Song(9856635, "Hostiles on the hill");
-        songs[2] = new Song(2462358, "Chocolate Lagoon");
-        songs[3] = new Song(27123148, "Not the Future");
-        songs[4] = new Song(138342537, "Seagulls");
-        songs[5] = new Song(29837715, "Not my stick");
-        songs[6] = new Song(32165042, "Carl Poppa");
-        songs[7] = new Song(8522785, "Sample of my pasta");
-        songs[8] = new Song(13413787, "Kicked your monkey");
-        songs[9] = new Song(1849229, "Don't stare at the sun");
-
+        // Initializing the first element
+        // of the array
+        List<CW1.Song> songs = List.of(
+                arr[0] = new Song(59966519, "Bushes of love", "Bad Lip Reading"),
+                arr[1] = new Song(9856635, "Hostiles on the hill", "Bad Lip Reading"),
+                arr[2] = new Song(2462358, "Chocolate Lagoon", "Bad Lip Reading"),
+                arr[3] = new Song(27123148, "Not the Future", "Bad Lip Reading"),
+                arr[4] = new Song(138342537, "Seagulls", "Bad Lip Reading"),
+                arr[5] = new Song(29837715, "Not my stick", "Bad Lip Reading"),
+                arr[6] = new Song(32165042, "Carl Poppa", "Bad Lip Reading"),
+                arr[7] = new Song(8522785, "Sample of my pasta", "Bad Lip Reading"),
+                arr[8] = new Song(13413787, "Kicked your monkey", "Bad Lip Reading"),
+                arr[9] = new Song(1849229, "Don't stare at the sun", "Bad Lip Reading"));
     }
 
-    /**
-    public static boolean doRemoveSong(){
-        System.out.println("Do you want to remove a song?");
-        int choice = InputReader.getInt("Yes=1 No=2");
-        boolean wantToRemoveSong = Boolean.parseBoolean(null);
-        switch(choice)
+
+
+
+    public  boolean removeSong()
+    {
+//Removing a song
+
+        int songNumber = arr.length +1;
+        while (songNumber< arr.length) {
+
+            System.out.println("Song " + songNumber+":");
+            songNumber = songNumber - 1;
+            arr[songNumber].display();
+        }
+        int numberOfSong = InputReader.getInt("What song number do you want to remove?");
+        int fixedNumberOfSong= numberOfRemovedSong();
+        arr[fixedNumberOfSong] = null;
+
+
+        return false;
+    }
+
+    public boolean addSong()
+    {
+        String newSongName = InputReader.getString("Enter song name: ");
+        String newArtistName = InputReader.getString("Enter artist name: ");
+
+        boolean NOPInputCheck = false;
+
+        int newNumberOfPlays = InputReader.getInt("Enter Number of Plays: ");
+        while(!NOPInputCheck)
         {
-            case 1: wantToRemoveSong = false; break;
-            case 2: wantToRemoveSong = true; break;
+
+            if(newNumberOfPlays < 0)
+            {
+                System.out.println("\n Invalid choice try again!\n");
+
+            }
+            else NOPInputCheck = true;
+                arr[arr.length-1] = new Song(newNumberOfPlays, newSongName, newArtistName);
+                printList();
+
         }
 
 
-        return wantToRemoveSong;
+        return NOPInputCheck;
     }
-
-    public static boolean addSong(){
-        System.out.println("Do you want to add a song?");
-        int choice = InputReader.getInt("Yes=1 No=2");
-        boolean wantToAddSong = Boolean.parseBoolean(null);
-        switch(choice)
-        {
-            case 1: wantToAddSong = false; break;
-            case 2: wantToAddSong = true; break;
-        }
-
-
-        return wantToAddSong;
-    }
-     */
     public static int numberOfRemovedSong()
     {
-        int numberOfRemovedSong = InputReader.getInt("What song number do you want to remove?");
-
-        return numberOfRemovedSong;
+        int numberOfSong = InputReader.getInt("What song number do you want to remove?");
+        int fixedSongNumber = numberOfSong-1;
+        return fixedSongNumber;
     }
+
+
+
+    /**Menu*/
+
+    private void menuSelection()
+    {
+        boolean programRunning = true;
+
+        while(programRunning == true)
+        {
+            int choice = menuInput();
+
+            switch(choice)
+            {
+                case 1: printList(); break;
+                case 2: addSong(); break;
+                case 3: removeSong(); break;
+                case 4: sortSongs(); break;
+                case 5: programRunning = false; break;
+            }
+        }
+    }
+
+    //Menu checker
+    private static int menuInput()
+    {
+        System.out.println(" \nMy song List\n");
+
+        int menuChoice = 0;
+        boolean menuInputCheck = false;
+
+        while(!menuInputCheck)
+        {
+            //The Menu
+            System.out.println("Menu:\n 1: Print song list");
+            System.out.println("2: Add song");
+            System.out.println("3: Remove song");
+            System.out.println("4: Filter List");
+            System.out.println("5: End Program");
+
+            menuChoice = InputReader.getInt("\n Please enter your choice (1 to 5) > ");
+
+            if(menuChoice < 1 || menuChoice > 5)
+            {
+                System.out.println("\n Invalid choice try again!\n");
+            }
+            else menuInputCheck = true;
+        }
+
+        return menuChoice;
+    }
+
+
 }
+
+
+
+/**In the friday practical sessing David helped me with some of the problems I was having with my code but it then turned out there was alot more wrong with it but in trying to fix it it was then unusable by me so I found a early verstion of my code on my home compluter.
+This was a preivous working vertion of my code that I used to do the testing but I ran out of time to fully compleate it */
