1d602ebf6d1e108597c9d78230b6d441477abb3d
Johnathan Barker <114746425+Johnathnat@users.noreply.github.com>
2022-11-07 13:25:02 +0000

    Update Song.java

--- src/CW1/Song.java
+++ src/CW1/Song.java
@@ -1,37 +1,48 @@
 package CW1;
-
-public class Song
-{
-    private int numberofSongs = 0;
-
-    int id;
+// Creating a Songs class with
+// id and songName as attributes
+class Song {
     public int numberOfPlays;
     public String songName;
-
-    public String artist;
+    public String artistName;
 
     // Songs class constructor
-    public Song(int numberOfPlays, String songName)
+    Song(int numberOfPlays, String songName, String artistName)
     {
-        numberofSongs++;
-        id = numberofSongs;
-
         this.numberOfPlays = numberOfPlays;
+        this.artistName = artistName;
         this.songName = songName;
-
     }
 
-    // display() method to display
-    // the Songs data
-    public void display() {
+    public void display()
+    {
 
-        System.out.println("Song name: " + songName + "\n" + "Amount of times played: " + numberOfPlays);
+        System.out.println("Song name: " + songName + "\n" + "By: "+ artistName +"\nAmount of times played: " + numberOfPlays);
         System.out.println();
 
     }
 
-//    public int getNumberofSongs()
-//    {
-//        return;
+     public String getSongName()
+    {
+        return songName;
+    }
+    public void setSongName(String name) {
+        this.songName = name;
+    }
+//    public static int getNumberOfPlays() {
+//        return numberOfPlays;
 //    }
+//
+//    public void getNumberOfPlays(int numberOfPlays) {
+//        this.numberOfPlays = numberOfPlays;
+//    }
+
+    // display() method to display
+    // the Songs data
+
+
+
+
 }
+/**In the friday practical sessing David helped me with some of the problems I was having with my code but it then turned out there was alot more wrong with it but in trying to fix it it was then unusable by me so I found a early verstion of my code on my home compluter.
+This was a preivous working vertion of my code that I used to do the testing but I ran out of time to fully compleate it */
